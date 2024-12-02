03be2f76f3782ab93f33e9c5cef269fb971a149e
toni-calvin <garcia.calvin.toni@gmail.com>
2024-12-02 04:19:33 +0100

    add diagnosis endpoints

---
+++ api/diagnoses/handlers.go
@@ -0,0 +1,87 @@
+package diagnoses
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+	"topdoctors/db"
+	"topdoctors/models"
+)
+
+type CreateDiagnosisRequest struct {
+	PatientID    string `json:"patient_id"`
+	Diagnosis    string `json:"diagnosis"`
+	Prescription string `json:"prescription"`
+	StartDate    string `json:"start_date"`
+}
+
+func GetDiagnoses(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	startDate := r.URL.Query().Get("start_date")
+
+	var diagnoses []models.Diagnosis
+	query := db.DB.Table("diagnoses").Joins("INNER JOIN patients ON patients.id = diagnoses.patient_id")
+
+	if name != "" {
+		query = query.Where("patients.name ILIKE ?", "%"+name+"%")
+	}
+	if startDate != "" {
+		query = query.Where("DATE(diagnoses.start_date) = ?", startDate)
+	}
+
+	if err := query.Find(&diagnoses).Error; err != nil {
+		http.Error(w, "Failed to fetch diagnoses", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(diagnoses)
+}
+
+func CreateDiagnosis(w http.ResponseWriter, r *http.Request) {
+	var req CreateDiagnosisRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println(req)
+	if req.PatientID == "" || req.Diagnosis == "" {
+		http.Error(w, "Missing required fields", http.StatusBadRequest)
+		return
+	}
+
+	var patient models.Patient
+	if err := db.DB.Where("id = ?", req.PatientID).First(&patient).Error; err != nil {
+		http.Error(w, "Patient not found", http.StatusNotFound)
+		return
+	}
+
+	var existingDiagnosis models.Diagnosis
+	if err := db.DB.Where("patient_id = ? AND diagnosis = ?", req.PatientID, req.Diagnosis).First(&existingDiagnosis).Error; err == nil {
+		http.Error(w, "Diagnosis already exists for this patient", http.StatusConflict)
+		return
+	}
+
+	layout := "2006-01-02" // Example format: YYYY-MM-DD
+	parsedDate, err := time.Parse(layout, req.StartDate)
+	if err != nil {
+		http.Error(w, "Invalid date format. Use YYYY-MM-DD", http.StatusBadRequest)
+		return
+	}
+
+	diagnose := models.Diagnosis{
+		PatientID:    req.PatientID,
+		Prescription: req.Prescription,
+		Diagnosis:    req.Diagnosis,
+		StartDate:    parsedDate,
+	}
+
+	if err := db.DB.Create(&diagnose).Error; err != nil {
+		http.Error(w, "Failed to create diagnosis", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(req)
+}
--- api/routes.go
+++ api/routes.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"topdoctors/api/auth"
+	"topdoctors/api/diagnoses"
 	"topdoctors/api/patients"
 	"topdoctors/api/users"
 
@@ -20,18 +21,17 @@ func SetupRoutes() *mux.Router {
 	internal.HandleFunc("/users", users.InternalCreateUserHandler).Methods("POST")
 	internal.HandleFunc("/patients", patients.CreatePatientHandler).Methods("POST")
 	internal.HandleFunc("/patients", patients.ListPatientsHandler).Methods("GET")
-
-	// internal.HandleFunc("/diagnoses", diagnoses.GetDiagnoses).Methods("GET")
-	// internal.HandleFunc("/diagnoses", diagnoses.CreateDiagnosis).Methods("POST")
+	internal.HandleFunc("/diagnoses", diagnoses.GetDiagnoses).Methods("GET")
+	internal.HandleFunc("/diagnoses", diagnoses.CreateDiagnosis).Methods("POST")
 
 	// External API
 	external := r.PathPrefix("/external").Subrouter()
 	external.Use(auth.AuthMiddleware("external"))
 	external.HandleFunc("/users", users.ExternalCreateUserHandler).Methods("POST")
+	external.HandleFunc("/patients", patients.CreatePatientHandler).Methods("POST")
 	external.HandleFunc("/patients", patients.ListPatientsHandler).Methods("GET")
-
-	// external.HandleFunc("/diagnoses", diagnoses.GetDiagnoses).Methods("GET")
-	// external.HandleFunc("/diagnoses", diagnoses.CreateDiagnosis).Methods("POST")
+	external.HandleFunc("/diagnoses", diagnoses.GetDiagnoses).Methods("GET")
+	external.HandleFunc("/diagnoses", diagnoses.CreateDiagnosis).Methods("POST")
 
 	return r
 }
